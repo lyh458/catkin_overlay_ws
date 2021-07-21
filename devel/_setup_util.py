c75f7dde883092b1a3347d2224ecb7ec60266f04
lyh458 <liyihui.ing@gmail.com>
2021-07-21 18:57:01 +0800

    rename ws name

--- devel/_setup_util.py
+++ devel/_setup_util.py
@@ -268,7 +268,7 @@ if __name__ == '__main__':
 
         if not args.local:
             # environment at generation time
-            CMAKE_PREFIX_PATH = r'/home/lyh/Codes/catkin_overlay_ws/devel;/home/lyh/Codes/tmp_ws/devel;/opt/ros/melodic'.split(';')
+            CMAKE_PREFIX_PATH = r'/home/lyh/Codes/catkin_overlay_ws/devel;/home/lyh/Codes/old_pack_ws/devel;/opt/ros/melodic'.split(';')
         else:
             # don't consider any other prefix path than this one
             CMAKE_PREFIX_PATH = []
